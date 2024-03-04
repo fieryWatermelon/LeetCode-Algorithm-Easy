89f8ecd60cf5a50767461c29f5dd8eca5abf29d1
fieryWatermelon <47157752+fieryWatermelon@users.noreply.github.com>
2024-03-04 20:31:19 +0100

    Update 001_Two_Sum.py

--- python/001_Two_Sum.py
+++ python/001_Two_Sum.py
@@ -1,4 +1,5 @@
 class Solution(object):
+    #暴力双for就好
     # def twoSum(self, nums, target):
     #     """
     #     :type nums: List[int]
@@ -13,49 +14,19 @@ class Solution(object):
     #                 return [i, j]
 
     # def twoSum(self, nums, target):
-    #     # hash 1
-    #     hash_nums = {}
-    #     for index, num in enumerate(nums):
-    #         try:
-    #             hash_nums[num].append(index)
-    #         except KeyError:
-    #             hash_nums[num] = [index]
-    #     for index, num in enumerate(nums):
-    #         another = target - num
-    #         try:
-    #             candicate = hash_nums[another]
-    #             if another == num:
-    #                 if len(candicate) > 1:
-    #                     return candicate
-    #                 else:
-    #                     continue
-    #             else:
-    #                 return [index, candicate[0]]
-    #         except KeyError:
-    #             pass
-
-    # def twoSum(self, nums, target):
     #     # hash 2
-    #     hash_nums = {}
-    #     for index, num in enumerate(nums):
-    #         another = target - num
-    #         try:
-    #             hash_nums[another]
-    #             return [hash_nums[another], index]
-    #         except KeyError:
-    #             hash_nums[num] = index
+    # enumerate: Return type: <class 'enumerate'>
+#[(0, 'eat'), (1, 'sleep'), (2, 'repeat')]
+#[(2, 'g'), (3, 'e'), (4, 'e'), (5, 'k')]
 
-    def twoSum(self, nums, target):
-        # two point
-        nums_index = [(v, index) for index, v in enumerate(nums)]
-        nums_index.sort()
-        begin, end = 0, len(nums) - 1
-        while begin < end:
-            curr = nums_index[begin][0] + nums_index[end][0]
-            if curr == target:
-                return [nums_index[begin][1], nums_index[end][1]]
-            elif curr < target:
-                begin += 1
-            else:
-                end -= 1
+
+    #   num_indices = {}
+        
+    #   for i, num in enumerate(nums):
+    #       complement = target - num
+            
+    #       if complement in num_indices:
+    #           return [num_indices[complement], i]
+
+    #       num_indices[num] = i
                 
